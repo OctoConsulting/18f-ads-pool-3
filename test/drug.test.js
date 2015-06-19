dce99a0d17c3550e932080267c4a3fa158de0cce
Tarandeep Dhupar <me@mydomain.com>
2015-06-19 19:09:05 -0400

    Test home.js changes

--- test/drug.test.js
+++ test/drug.test.js
@@ -2,10 +2,28 @@ var app = require('../server/server.js');
 var should = require('should');
 var supertest = require('supertest');
 
-describe('Test Drug', function(){
-	 describe('Test findSuggestions', function(){
-	 	it('API Test', function(done){
-	 		done();
+describe('Test Drug Model', function(){
+	 describe('Test REST API - suggestions', function(){
+	 	it('Results exists', function(done){
+	 		supertest(app).get("/api/drugs/suggestions?q=Tyl").expect(200).end(function(err,res){
+		 		if(err) throw err;
+		 		res.status.should.equal(200);
+		 		var responseOBJ = res.body;	
+		 		console.log('responseOBJ:::'+responseOBJ);
+		 		//responseOBJ.result.length.should.notEqual(0);
+		 		done();
+	 		});	
 	 	});
+
+	 	it("Results doesn't exist", function(done){
+	 		supertest(app).get("/api/drugs/suggestions?q=XXXX").expect(200).end(function(err,res){
+		 		if(err) throw err;
+		 		res.status.should.equal(200);
+		 		var responseOBJ = res.body;	
+		 		responseOBJ.result.length.should.equal(0);
+		 		done();
+	 		});	
+	 	});
+
 	 });
 });
