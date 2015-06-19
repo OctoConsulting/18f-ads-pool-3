3dd7b19b7b95670e1e305804171656b824e8032b
sonimehul <soni.mehul@octoconsulting.com>
2015-06-19 17:08:14 -0400

    Utils functions

---
+++ utils.js
@@ -0,0 +1,18 @@
+var toString = module.exports.getToString = function (str) {
+    if(!str) return null;
+    var lastChar = str.charCodeAt(str.length-1);
+
+    var nextChar = String.fromCharCode(lastChar+1);
+    var newString = str.slice(0,str.length-1) + nextChar;    
+    
+    return newString;
+};
+
+
+module.exports.getSearchQuery = function (str) {
+   var result = 'https://api.fda.gov/drug/label.json?api_key=yiv5ZoikJg3kSSZ5edvsiqnJa9yvHoxrm6EWT8yi&search=openfda.brand_name:';
+   var range = '[' + str + '+TO+' + toString(str) + ']';
+   result = result + range + '+OR+openfda.generic_name:' + range +'&limit=10';
+
+   return result;
+};
