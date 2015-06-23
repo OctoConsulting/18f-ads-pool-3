660518ebe1de7380cfbb0460bfa4f26ef61dd103
Dave Reed <dave@contagious.ly>
2015-06-23 13:01:18 -0400

    Added the recall and event counts to the tabs

--- client/src/app/views/details/details.js
+++ client/src/app/views/details/details.js
@@ -22,6 +22,12 @@
                 resolve: {
                     detailsData: function(Restangular, $stateParams) {
                         return Restangular.one('drugs').customGET('details',{'q':$stateParams.name,'typ':$stateParams.typ});
+                    },
+                    eventsData: function(Restangular, $stateParams) {
+                        return Restangular.one('events').customGET('',{'q':$stateParams.name,'typ':$stateParams.typ,'limit':5,'skip':0});
+                    },
+                    recallsData: function(Restangular, $stateParams) {
+                        return Restangular.one('recalls').customGET('',{'q':$stateParams.name,'typ':$stateParams.typ,'limit':5,'skip':0});
                     }
                 }
             })
@@ -29,10 +35,14 @@
         })
         .controller( 'DetailsController', DetailsController);
 
-        function DetailsController($scope, Restangular, $state, detailsData, $stateParams) {
+        function DetailsController($scope, Restangular, $state, detailsData, $stateParams, eventsData, recallsData) {
             $scope.details = detailsData;
             $scope.indicator = $stateParams.typ;
             $scope.name = $stateParams.name;
+            $scope.tabState = 'recalls';
+            $scope.events = eventsData;
+            $scope.recalls = recallsData;
+
         }
 })();
 
